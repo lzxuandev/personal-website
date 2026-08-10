78ccab652d200a5bf944145d73f9261726e367ce
lzx <168624142+lzxuandev@users.noreply.github.com>
2026-08-11 04:18:02 +0800

    Update list.js

--- assets/js/pages/list.js
+++ assets/js/pages/list.js
@@ -299,7 +299,7 @@
         { title: 'Nuvole Bianche', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQXbz-sGrdwJ9AmWPRr4iDp2MWJVoEefLpn_9O8ddNGQ&s=10' },
         { title: 'Canon in D', image: 'https://i1.sndcdn.com/artworks-000222653975-rwu5fb-t500x500.jpg' },
         { title: 'Mariage d\'Amour', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtZ-FDkU9NqHsKX8S6vlegaMW6x4TNKD48Is-6NmVvyg&s=10' },
-        { title: '最伟大的作品 - 周杰伦', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI_eIUut7SSuwWsUFaJxgU21gR7HZOUoM0TCvG3d3fiw&s=10' },
+        { title: '最伟大的作品', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTI_eIUut7SSuwWsUFaJxgU21gR7HZOUoM0TCvG3d3fiw&s=10' },
         { title: 'River Flows in You', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSNOLS5zAlJQeBQlICLt4eFSiLRGgcikdGwZMNPukpfFA&s=10' },
         { title: 'Clair de Lune', image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRObwVTCPD1b0DbxGcHPhzohbUQfh4W2RQLsa126le1lg&s=10' },
       ],
